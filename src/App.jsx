1c38a8096e16e84eb0ea01517f2b55c73a1c3f46
zhangyue <zhangyuea@hualala.com>
2023-02-23 18:53:02 +0800

    feat#跨域，配置代理的第一种方法

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>App...</div>
-    )
-  }
+    state = {
+        students: []
+    }
+    getStudentData = () => {
+        axios.get('http://localhost:3000/students.html').then(
+            response => {
+                console.log('成功了', response.data);
+                this.setState({
+                    students: response.data
+                });
+            },
+            error => {console.log('失败了', error)}
+        );
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.getStudentData}>点我获取学生数据</button>
+                {this.state.students.map((student, index) => {
+                    return (
+                        <div key={student.id}>
+                            <span>id: {student.id}</span>&nbsp;
+                            <span>name: {student.name}</span>&nbsp;
+                            <span>age: {student.age}</span>
+                        </div>
+                    );
+                })}
+            </div>
+        )
+    }
 }
