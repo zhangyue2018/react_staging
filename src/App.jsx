0420df5f929fbd10c919ae17e2a0f8e833ae0965
zhangyue <zhangyuea@hualala.com>
2023-03-08 17:40:07 +0800

    redirect重定向--默认路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch,Redirect } from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
 import Header from './components/Header';
@@ -20,16 +20,17 @@ export default class App extends Component {
                     <div className='list-group'>
                         {/* <NavLink activeClassName='selected' className='list-group-item' to='/about'>About==</NavLink> */}
                         {/* <NavLink activeClassName='selected' className='list-group-item' to='/home'>Home==</NavLink> */}
-                        <MyNavLink to='/zy/about'>About</MyNavLink>
-                        <MyNavLink to='/zy/home'>Home</MyNavLink>
+                        <MyNavLink to='/about'>About</MyNavLink>
+                        <MyNavLink to='/home'>Home</MyNavLink>
                     </div>
                 </div>
                 <div className='col-xs-6'>
                     <div className='panel'>
                         <div className='panel-body'>
                             <Switch>
-                                <Route exact={true} path='/zy/about' component={About}/>
-                                <Route exact path='/zy/home' component={Home}/>
+                                <Route exact={true} path='/about' component={About}/>
+                                <Route exact path='/home' component={Home}/>
+                                <Redirect to="/home" />
                             </Switch>
                         </div>
                     </div>
