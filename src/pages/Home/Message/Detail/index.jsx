447b3811e6e75263aa5ad01e3b4c4b4312c44c36
zhangyue <zhangyuea@hualala.com>
2023-03-09 18:47:29 +0800

    向路由组件传递参数---search参数

--- src/pages/Home/Message/Detail/index.jsx
+++ src/pages/Home/Message/Detail/index.jsx
@@ -1,9 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import qs from 'query-string';
 
 export default class Detail extends Component {
   render() {
     // 接收params参数
-    const {id, title, content} = this.props.match.params;
+    // const {id, title, content} = this.props.match.params;
+    const search = this.props.location.search;
+    const obj = qs.parse(search.slice(1));
+    const {id, title, content} = obj;
     return (
       <ul>
         <li>id: {id}</li>
--- src/pages/Home/Message/index.jsx
+++ src/pages/Home/Message/index.jsx
@@ -21,14 +21,18 @@ export default class Message extends Component {
                             return (
                                 <li key={message.id}>
                                     {/* 向路由组件传递params参数 */}
-                                    <Link to={`/home/message/detail/${message.id}/${message.title}/${message.content}`}>{message.title}</Link>
+                                    {/* <Link to={`/home/message/detail/${message.id}/${message.title}/${message.content}`}>{message.title}</Link> */}
+                                    {/**想路由组件传递search参数 */}
+                                    <Link to={`/home/message/detail/?id=${message.id}&title=${message.title}&content=${message.content}`}>{message.title}</Link>
                                 </li>
                             ) 
                         })
                     }
                 </ul>
                 {/** 声明接收params参数 */}
-                <Route path='/home/message/detail/:id/:title/:content' component={Detail}/>
+                {/* <Route path='/home/message/detail/:id/:title/:content' component={Detail}/> */}
+                {/** search参数不用声明接收，正常注册路由即可 */}
+                <Route path='/home/message/detail' component={Detail}/>
             </div>
         )
     }
