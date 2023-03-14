4e46e1366f611cb5c0c2833e3464f27238e26df6
zhangyue <zhangyuea@hualala.com>
2023-03-14 15:52:33 +0800

    求和案例的react版本

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import Count from './components/Count';
 export default class App extends Component {
   render() {
     return (
         <div>
-            App...
+            <Count />
         </div>
     )
   }
