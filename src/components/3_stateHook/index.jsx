be73f7ac57f70bbf49d6d51a002ac05669e4d7d7
zhangyue <zhangyuea@hualala.com>
2023-03-20 18:29:16 +0800

    RefHook

--- src/components/3_stateHook/index.jsx
+++ src/components/3_stateHook/index.jsx
@@ -1,12 +1,54 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import root from '../../index';
+
+
+// class Demo extends React.Component {
+
+//     state = { count: 0 }
+
+//     myRef = React.createRef();
+
+//     add = () => {
+//         this.setState(state => ({ count: state.count + 1 }));
+//     }
+
+//     unmount = () => {
+//         root.unmount(document.getElementById('root'));
+//     }
+
+//     show = () => {
+//         alert(this.myRef.current.value);
+//     }
+
+//     componentDidMount() {
+//         this.timer = setInterval(() => {
+//             this.setState(state => ({ count: state.count + 1 }))
+//         }, 1000);
+//     }
+
+//     componentWillUnmount() {
+//         clearInterval(this.timer)
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <input type="text" ref={this.myRef}/>
+//                 <div>当前求和为：{this.state.count}</div>
+//                 <button onClick={this.add}>点我+1</button>
+//                 <button onClick={this.unmount}>卸载组件</button>
+//                 <button onClick={this.show}>显示输入</button>
+//             </div>
+//         )
+//     }
+// }
+
 
 function Demo() {
     let [sum, setCount] = React.useState(0);  // react底层做了处理，第一次调用后，sum被存储，之后重新渲染再次调用时，不会覆盖已存储的值
     const [name, setName] = React.useState('zy');
 
-    let timer = null;
+    let myRef = React.useRef();
 
     /**
      * React.useEffect中的第一个传参是函数，此函数在组件加载之后必定会被调用
@@ -27,7 +69,7 @@ function Demo() {
 
     function add() {
         // setCount(sum+1); // 第一种写法,参数为非函数值，直接指定新的状态值，内部用其覆盖原来的状态值
-        setCount(count => count +1); // 第二种写法，参数为函数，接收原本的状态值，返回新的状态值，内部用其覆盖原来的状态值
+        setCount(count => count + 1); // 第二种写法，参数为函数，接收原本的状态值，返回新的状态值，内部用其覆盖原来的状态值
     }
 
     function change() {
@@ -38,13 +80,19 @@ function Demo() {
         root.unmount(document.getElementById('root'));
     }
 
+    function show() {
+        alert(myRef.current.value)
+    }
+
     return (
         <div>
+            <input ref={myRef} type="text" />
             <div>当前求和为：{sum}</div>
             <div>我的名字是：{name}</div>
             <button onClick={add}>点我+1</button>
             <button onClick={change}>点我改名</button>
             <button onClick={unmount}>卸载组件</button>
+            <button onClick={show}>点我提示数据</button>
         </div>
     );
 }
