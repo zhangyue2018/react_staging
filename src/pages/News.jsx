458431f729e8d06ca284e4e8b0c5877bf66be5fc
zhangyue <zhangyuea@hualala.com>
2023-03-24 15:11:58 +0800

    useNavgationType----react-router-6

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
-
+import React from 'react';
+import {useNavigationType} from 'react-router-dom';
+/**
+ * useNavigationType:返回当前的导航类型（用户是如何来到当前页面的）
+ * 返回值：POP、PUSH、REPLACE
+ * 注：POP是指在浏览器中直接打开了这个路由组件（刷新页面）
+ */
 export default function News() {
+    console.log(useNavigationType());
   return (
     <div>
         <ul>
